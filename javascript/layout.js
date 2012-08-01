05659bc587cabbcf18cb812a18a907262b18b0d9
libra guest <ssylvia@esri.com>
2012-08-01 13:03:12 -0400

    first draft

--- javascript/layout.js
+++ javascript/layout.js
@@ -1,3 +1,6 @@
+var _swipe;
+var _autoSlide = true;
+
 $(document).ready(function(){
     $(".menuHeader").click(function(){
         if($(this).next().hasClass("menuContent")){
@@ -10,4 +13,47 @@ $(document).ready(function(){
             $(this).next().stop(true,true).slideToggle();
         }
     });
+
+    $(".collapse").click(function(){
+        $(this).parent().addClass("closed").stop(true,true).slideUp().prev().children(".toggleIcon").html("+");
+    });
+
+    _swipe = new Swipe(document.getElementById('slider'), {
+        startSlide: 0,
+        speed: 400,
+        callback: function(e, pos) {
+            $(".pos").removeClass('on');
+            $("#pos"+pos).addClass('on');
+        }
+    });
+
+    startAutoSlide();
+
+    $(".pos").click(function(){
+        _swipe.slide($(this).index(),400);
+        _autoSlide = false;
+    });
+
+    $("#slider").swipe(function(){
+        _autoSlide = false;
+    });
+    
+    $("#prevArrowMain").click(function(){
+        _autoSlide = false;
+        _swipe.prev();
+    });
+    $("#nextArrowMain").click(function(){
+        _autoSlide = false;
+        _swipe.next();
+    });
+
 });
+
+var startAutoSlide = function(){
+    setTimeout(function() {
+        if (_autoSlide === true){
+            _swipe.next();
+            startAutoSlide();
+        }
+    }, 5000);
+};
